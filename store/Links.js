11fe0fa91abb8d2570830cc10e024bee2ca89d70
Hunter Feltes <Jhfeltes@gmail.com>
2019-05-20 19:22:12 -0500

    Update nav to spit out text

--- store/Links.js
+++ store/Links.js
@@ -1,5 +1,5 @@
 export default {
     'primary': [
-      { 'text': 'Home', 'icon': 'fas fa-atom' }, {'text': 'About', 'icon': 'fas fa-allergies'}{ 'text': 'Contact', 'icon': 'fab fa-android'} ],
-    'dropdown': [ 'Class Project', 'Hack-a-Thon', 'Demo Day' ]
+        { 'text': 'Home', 'icon': 'fas fa-atom' }, { 'text': 'About', 'icon': 'fas fa-allergies' }, { 'text': 'Contact', 'icon': 'fab fa-android' } ],
+    'dropdown': [ { 'text': 'Class Project' } , { 'text': 'Hack-a-Thon' } , { 'text': 'Demo Day' } ]
 };
