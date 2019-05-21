14cbb80edffe7c478f59df6facd646932de0c66a
Hunter Feltes <Jhfeltes@gmail.com>
2019-05-20 19:06:32 -0500

    Added icons to Navigation

---
+++ store/Links.js
@@ -0,0 +1,5 @@
+export default {
+    'primary': [
+      { 'text': 'Home', 'icon': 'fas fa-atom' }, {'text': 'About', 'icon': 'fas fa-allergies'}{ 'text': 'Contact', 'icon': 'fab fa-android'} ],
+    'dropdown': [ 'Class Project', 'Hack-a-Thon', 'Demo Day' ]
+};
