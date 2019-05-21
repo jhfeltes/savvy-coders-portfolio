8d61f99eb1a05d9b122a093285bf5ed18f447e9b
Hunter Feltes <Jhfeltes@gmail.com>
2019-05-21 15:34:22 -0500

    moved pages folder into components folder

--- pages/About.js
+++
@@ -1,5 +0,0 @@
-
-export default function(){
-    return `<h1>About Me!</h1>
-<p>Go Blues</p>`;
-}
--- pages/index.js
+++
@@ -1,5 +0,0 @@
-// Grab whatever Home.js is exporting by default.
-// Export this under the name 'Home.'
-export { default as Home } from './Home';
-export { default as About } from './About';
-export { default as Contact } from './Contact';
