b8932dcbbc421b61cefa789cf9f880f93e2cac6e
Hunter Feltes <Jhfeltes@gmail.com>
2019-05-29 18:59:07 -0500

    add blogs store data

---
+++ store/Blog.js
@@ -0,0 +1,10 @@
+// import img from '../img/sample.jpeg';
+import links from './Links';
+
+export default {
+    // Value of pageContent corresponds with one of the modules containted in Pages
+    'pageContent': 'Blog',
+    'title': 'The Blog',
+    'links': links
+    'posts':[]
+};
