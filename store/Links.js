8efde7849306bc56d70193c07d40f28dea875a18
Hunter Feltes <Jhfeltes@gmail.com>
2019-05-29 19:01:08 -0500

    add blof to menu

--- store/Links.js
+++ store/Links.js
@@ -1,5 +1,5 @@
 export default {
     'primary': [
-        { 'text': 'Home', 'icon': 'fas fa-atom' }, { 'text': 'About', 'icon': 'fas fa-allergies' }, { 'text': 'Contact', 'icon': 'fab fa-android' } ],
+        { 'text': 'Home', 'icon': 'fas fa-atom' }, { 'text': 'About', 'icon': 'fas fa-allergies' }, { 'text': 'Contact', 'icon': 'fab fa-android' }, { 'text': 'Blog' } ],
     'dropdown': [ { 'text': 'Class Project' } , { 'text': 'Hack-a-Thon' } , { 'text': 'Demo Day' } ]
 };
